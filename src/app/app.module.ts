2bfb43ae4e2ecf3feb9ce6d7c16a7f164c2493af
Stuart Wallace <swallace@onbase.net>
2018-07-09 14:39:31 -0400

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TreeComponent } from './tree/tree.component';
 import { NodesComponent } from './nodes/nodes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeComponent,
     NodesComponent
   ],
   imports: [
